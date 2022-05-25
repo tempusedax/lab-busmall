aa12a205a40f2e334bcb3d6cef594fbe15fe9a61
oik <oik@Merohingi-MacBook-Pro.local>
2022-05-25 00:54:52 -0500

    done but something is off

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 'use strict';
 
+const ctx = document.getElementById('myChart').getContext('2d');
 let imgContainer = document.getElementById('img-container');
 let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
-let imgThree= document.getElementById('img-three');
+let imgThree = document.getElementById('img-three');
 let showResultsBtn = document.getElementById('show-results-btn');
 let resultsList = document.getElementById('results-list');
 
 let voteCount = 25;
 let allAdverts = [];
+let indexArray = [];
 
 
-function Advert(name, fileExtension = 'jpg'){
+function Advert(name, fileExtension = 'jpg') {
   this.name = name;
   this.views = 0;
   this.votes = 0;
@@ -40,69 +42,123 @@ new Advert('water-can');
 new Advert('wine-glass');
 
 
-function getRandomIndex(){
-  return Math.floor(Math.random()*allAdverts.length);
+function getRandomIndex() {
+  return Math.floor(Math.random() * allAdverts.length);
 }
-
-function renderImgs(){
-  let randOptions = []
-  let imgOneIndex = getRandomIndex();
-  let imgTwoIndex = getRandomIndex();
-  let imgThreeIndex = getRandomIndex();
 
   // NOTE: Your lab will require you to have 3 unique images per round
   // HINT: Consider using a container to store your random indexes and then validate if there are 3 unique numbers in that container
+  // while (imgOneIndex === imgTwoIndex || imgOneIndex === imgThreeIndex || imgTwoIndex === imgThreeIndex) {
+  //   imgTwoIndex = getRandomIndex();
+  //   imgThreeIndex = getRandomIndex();
+  // }
+  function renderImgs() {
+    while(indexArray.length < 6){
+      let num = getRandomIndex();
+      if(!indexArray.includes(num)){
+        indexArray.push(num);
+      }
+    }
+  
 
-while(imgOneIndex === imgTwoIndex){
-  imgTwoIndex = getRandomIndex();
+  let imgOneIndex = indexArray.shift();
+  let imgTwoIndex = indexArray.shift();
+  let imgThreeIndex = indexArray.shift(); 
+
+  imgOne.src = allAdverts[imgOneIndex].photo;
+  imgOne.alt = allAdverts[imgOneIndex].name;
+  allAdverts[imgOneIndex].views++;
+
+  imgTwo.src = allAdverts[imgTwoIndex].photo;
+  imgTwo.alt = allAdverts[imgTwoIndex].name;
+  allAdverts[imgTwoIndex].views++;
+
+  imgThree.src = allAdverts[imgThreeIndex].photo;
+  imgThree.alt = allAdverts[imgThreeIndex].photo;
+  allAdverts[imgThreeIndex].views++;
+
+  if (voteCount === 0) {
+    imgContainer.removeEventListener('click', handleClick);
+  }
 }
-
-imgOne.src = allAdverts[imgOneIndex].photo;
-imgOne.alt = allAdverts[imgOneIndex].name;
-allAdverts[imgOneIndex].views++;
-
-imgTwo.src = allAdverts[imgTwoIndex].photo;
-imgTwo.alt = allAdverts[imgTwoIndex].name;
-allAdverts[imgTwoIndex].views++;
-
-imgThree.src = allAdverts[imgThreeIndex].photo;
-imgThree.alt = allAdverts[imgThreeIndex].photo;
-allAdverts[imgThreeIndex].views++;
-
-}
+// }
 
 renderImgs();
 
 // ********* EVENT HANDLERS *******************
 function handleClick(event) {
-voteCount--;
+  voteCount--;
 
-let imgClicked = event.target.alt;
+  let imgClicked = event.target.alt;
 
-for(let i = 0; i < allAdverts.length; i++){
-  if(imgClicked === allAdverts[i].name){
-    allAdverts[i].votes++;
+  for (let i = 0; i < allAdverts.length; i++) {
+    if (imgClicked === allAdverts[i].name) {
+      allAdverts[i].votes++;
+    }
   }
-}
-//rerender 2 new goat images
-renderImgs();
-
-// once voting rounds completed - stop clicks
-if(voteCount === 0){
-  imgContainer.removeEventListener('click', handleClick);
-
+  //rerender 2 new images
+  renderImgs();
+  // once voting rounds completed - stop clicks
 }
 
-}
+function renderChart() {
+  let picName = [];
+  let picVote = [];
+  let picViews = [];
 
-function handleShowResults(){
-if(voteCount === 0){
-  for(let i = 0; i < allAdverts.length; i++){
-    let liElement = document.createElement('li');
-    liElement.textContent = `${allAdverts[i].name} was shown ${allAdverts[i].views} times and voted for ${allAdverts[i].votes} times.`;
-    resultsList.appendChild(liElement);
+  for (let i = 0; i < allAdverts.length; i++) {
+
+    picName.push(allAdverts[i].name);
+    picVote.push(allAdverts[i].vote);
+    picViews.push(allAdverts[i].views);
   }
+
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: picName,
+      datasets: [{
+        label: '# of Votes',
+        data: picVote,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: picViews,
+        backgroundColor: [
+          'rgba(155, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
+
+function handleShowResults() {
+  if (voteCount === 0) {
+    renderChart();
+    for (let i = 0; i < allAdverts.length; i++) {
+      let liElement = document.createElement('li');
+      liElement.textContent = `${allAdverts[i].name} was shown ${allAdverts[i].views} times and voted for ${allAdverts[i].votes} times.`;
+      resultsList.appendChild(liElement);
+    }
+  }
 }
 
 
